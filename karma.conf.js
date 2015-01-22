daa38643acbd3158ce2b95a7e9d362538061c953
Stef Hock <hock@planet.nl>
2015-01-23 00:27:51 +0100

    Disabling coverage reporting because it cannot work with generator methods (needs harmony branch)

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'app/src/oig/**/*.js': 'coverage'
+      //'app/src/oig/**/*.js': 'coverage'
     },
 
     // list of files / patterns to load in the browser
