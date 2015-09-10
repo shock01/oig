1cad904db6424f75e84453fada2193f1814d3b9a
Nagogus <nagoga.ruben@gmail.com>
2015-09-10 21:14:49 +0200

    Move bootstrap logic in bootstrap.js. Rework demo to use oigBootstrap

--- test/it/bootstrapSpec.js
+++ test/it/bootstrapSpec.js
@@ -6,54 +6,6 @@ describe("bootsrap", function() {
     view1,
     viewContext;
 
-  function bootstrap(element) {
-    parse(element);
-    observe(element)
-  }
-
-  function observe(document) {
-    var viewContext = oig.locator.resolve('oigViewContext');
-    var observer = new MutationObserver(function(mutations) {
-      mutations.forEach(function(mutation) {
-        var i = 0,
-          node;
-        while(node = mutation.removedNodes[i++]) {
-          dispose(node);
-        }
-        i = 0;
-        while(node = mutation.addedNodes[i++]) {
-          parse(node);
-        }
-      });
-    });
-    // pass in the target node, as well as the observer options
-    observer.observe(document, { attributes: false, childList: true, characterData: false, subtree: true });
-  }
-
-  function dispose(element) {
-    var viewContext = oig.locator.resolve('oigViewContext');
-    var treeWalker = document.createTreeWalker(element, NodeFilter.SHOW_ELEMENT, null, false);
-    while(treeWalker.nextNode()) {
-      viewContext.dispose(treeWalker.currentNode);
-    }
-    viewContext.dispose(element);
-  }
-
-  function parse(element) {
-
-    var viewContext = oig.locator.resolve('oigViewContext');
-    var treeWalker = document.createTreeWalker(element, NodeFilter.SHOW_ELEMENT, {
-      acceptNode: function (node) {
-        if(node.hasAttribute('data-oig-viewmodel')) {
-          return NodeFilter.FILTER_ACCEPT;
-        }
-      }
-    }, false);
-    while(treeWalker.nextNode()) {
-      viewContext.init(treeWalker.currentNode);
-    }
-  }
-
   function attachViewModel1AndView1() {
     function viewModel1() { this.viewModelProp = 123; }
     function view1() { this.viewProp = 123; }
@@ -64,7 +16,7 @@ describe("bootsrap", function() {
     document.body.appendChild(element);
     oig.bind('viewModel1').to(viewModel1);
     oig.bind('view1').to(view1);
-    bootstrap(document.body);
+    oig.bootstrap.run(document.body);
     viewContext = oig.locator.resolve('oigViewContext');
   }
 
@@ -75,7 +27,7 @@ describe("bootsrap", function() {
     element.setAttribute(OigAttrs.VIEWMODEL, 'viewModel1');
     document.body.appendChild(element);
     oig.bind('viewModel1').to(viewModel1);
-    bootstrap(document.body);
+    oig.bootstrap.run(document.body);
     viewContext = oig.locator.resolve('oigViewContext');
   }
 
