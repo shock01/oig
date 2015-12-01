637af455525e3a7ac478244528f02eaf7b0fe330
Stef Hock <spwhock@gmail.com>
2015-12-01 21:33:29 +0100

    Fixed jshint warnings

--- app/src/oig/polyfills.js
+++ app/src/oig/polyfills.js
@@ -2,46 +2,55 @@
 * contains polyfill for es5 features which have not been implemented fully or
 * that are broken (CustomEvent in IE)
 */
-
+  'use strict';
 // polyfill window.event for firefox, also use strict cannot use arguments.callee.caller.arguments[0] to get event
-if (!('event' in window)) {
-  var currentEvent,
-    keys = Object.keys(window),
-    length = keys.length,
-    key;
+(function() {
+  if (!('event' in window)) {
+    var currentEvent,
+      keys = Object.keys(window),
+      length = keys.length,
+      key;
 
-  var eventListener = function( /**Event*/ event) {
-    currentEvent = event;
-  };
-
-  while (length > 0) {
-    key = keys[--length];
-    if (key.substring(0, 2) === 'on') {
-      window.addEventListener(key.substr(2), eventListener, true);
-    }
-  }
-  Object.defineProperty(window, 'event', {
-    get: function() {
-      return currentEvent;
-    },
-    set: function (event) {
+    var eventListener = function( /**Event*/ event) {
       currentEvent = event;
+    };
+
+    while (length > 0) {
+      key = keys[--length];
+      if (key.substring(0, 2) === 'on') {
+        window.addEventListener(key.substr(2), eventListener, true);
+      }
     }
-  });
-}
+    Object.defineProperty(window, 'event', {
+      get: function() {
+        return currentEvent;
+      },
+      set: function(event) {
+        currentEvent = event;
+      }
+    });
+  }
+}())
+
 // polyfill for CustomEvent for PhantomJS and IE
-(function () {
-  if (typeof CustomEvent === 'function') { return; }
+(function() {
+  if (typeof CustomEvent === 'function') {
+    return;
+  }
   function customEvent(event, params) {
-      params = params || {bubbles: false, cancelable: false, detail: undefined};
-      var evt = document.createEvent('CustomEvent');
-      evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-      return evt;
+    params = params || {
+        bubbles: false,
+        cancelable: false,
+        detail: undefined
+      };
+    var evt = document.createEvent('CustomEvent');
+    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+    return evt;
   }
   customEvent.prototype = window.Event.prototype;
   window.CustomEvent = customEvent;
 })();
-
+/* jshint ignore:start */
 /**
  * @license
  * Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
@@ -94,3 +103,4 @@ if (typeof WeakMap === 'undefined') {
     window.WeakMap = WeakMap;
   })();
 }
+/* jshint ignore:end */
